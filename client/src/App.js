90b5821c1fcd214834ba9f4450ffc647d6521282
alexangria1992 <alex040892@gmail.com>
2022-02-19 12:54:38 +1300

    initialized server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./responsive.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SingleProduct from "./screens/SingleProduct";
