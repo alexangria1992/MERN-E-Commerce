12e23aaad09430315797cb5475e4b9065a1cea88
alexangria1992 <alex040892@gmail.com>
2022-02-18 22:01:05 +1300

    rest of frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SingleProduct from "./screens/SingleProduct";
 import Login from "./screens/Login";
+import ProfileScreen from "./screens/ProfileScreen";
+import OrderScreen from "./screens/OrderScreen";
+import CartScreen from "./screens/CartScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import Register from "./screens/Register";
+import NotFound from "./screens/NotFound";
 
 function App() {
   return (
@@ -11,6 +19,15 @@ function App() {
         <Route path="/" component={HomeScreen} exact />
         <Route path="/products/:id" component={SingleProduct} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+
+        <Route path="/profile" component={ProfileScreen} />
+        <Route path="/cart/:id?" component={CartScreen} />
+        <Route path="/order" component={OrderScreen} />
+        <Route path="/shipping" component={ShippingScreen} />
+        <Route path="/payment" component={PaymentScreen} />
+        <Route path="/placeorder" component={PlaceOrderScreen} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </Router>
   );
