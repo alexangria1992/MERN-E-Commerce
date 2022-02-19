248c1cd7c93d329ddc7cedd103d5ab533821e8cc
alexangria1992 <alex040892@gmail.com>
2022-02-19 21:11:19 +1300

    fetched single products from backend

--- client/src/screens/SingleProduct.js
+++ client/src/screens/SingleProduct.js
@@ -1,14 +1,22 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../components/homeComponents/Rating";
 import Header from "./../components/Header";
 import Message from "./../components/LoadingError/Error";
-import products from "../data/Products";
-import { useParams } from "react-router-dom";
 
 const SingleProduct = ({ match }) => {
-  const { id } = useParams();
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchproduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchproduct();
+  }, []);
   return (
     <>
       <Header />
