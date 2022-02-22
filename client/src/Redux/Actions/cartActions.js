7369083558cee4faacb4a186d0d3b609cf8b0810
alexangria1992 <alex040892@gmail.com>
2022-02-22 15:52:41 +1300

    Add quantity to cart

--- client/src/Redux/Actions/cartActions.js
+++ client/src/Redux/Actions/cartActions.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+import { CART_ADD_ITEM } from "../Constants/CartConstants";
 
-export const addtoCart = (id, qty) => async (dispatch, getState) => {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
-    type: CART_REMOVE_ITEM,
+    type: CART_ADD_ITEM,
     payload: {
       product: data._id,
       name: data.name,
       image: data.image,
       price: data.price,
       countInStock: data.countInStock,
-      qty: data.qty,
+      qty,
     },
   });
 
