e5afe618c953766716f7660d7f3dc8da225fdd76
alexangria1992 <alex040892@gmail.com>
2022-02-23 14:13:41 +1300

    Login API

--- Server/Models/UserModel.js
+++ Server/Models/UserModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -25,6 +25,11 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// LOGIN
+userSchema.methods.matchPassword = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
