675c2925821b244922526d5ce8c94e74bdc47e60
alexangria1992 <alex040892@gmail.com>
2022-02-23 15:59:49 +1300

    User Profile API Completed

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import protect from "../Middleware/AuthMiddleware.js";
 import generateToken from "../utils/generateToken.js";
 import User from "./../Models/UserModel.js";
 
@@ -28,4 +29,25 @@ userRouter.post(
   })
 );
 
+//PROFILE LOGIN
+userRouter.get(
+  "/profile",
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  })
+);
 export default userRouter;
