aa924ffd7eee78f5149b5721752d56248164bd11
alexangria1992 <alex040892@gmail.com>
2022-02-23 20:50:00 +1300

    Login Screen

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -6,21 +6,30 @@ import {
   productListReducer,
 } from "./Reducers/ProductReducers";
 import { cartReducer } from "./Reducers/CartReducers";
+import { userLoginReducer } from "./Reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+//LOGIN
+
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
   },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
 const middleware = [thunk];
